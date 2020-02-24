7b63addca486aa28aac06b62197eb958926d23ec
drakulan <drakulan-prestataire@ina.fr>
2020-02-24 14:15:55 +0100

    Fix sonar

--- src/app/core/mse/hls/hls-custom-f-loader.ts
+++ src/app/core/mse/hls/hls-custom-f-loader.ts
@@ -8,7 +8,6 @@ export class HlsCustomFLoader extends Hls.DefaultConfig.loader {
     constructor(config: Hls.LoaderConfig) {
         super(config);
         const load = this.load.bind(this);
-        const audioChannel = this._audioChannel;
         // tslint:disable-next-line:no-shadowed-variable
         this.load = (context, config, callbacks) => {
             context.url = context.url.replace(/(\/seg-\d+-v\d+-a)\d+(\.ts)/i, '$1' + this._audioChannel + '$2');
@@ -38,40 +37,3 @@ export class HlsCustomFLoader extends Hls.DefaultConfig.loader {
         this._audioChannel = value;
     }
 }
-
-
-// export class HlsCustomFLoader extends Hls.DefaultConfig.loader {
-//     constructor(config: Hls.LoaderConfig) {
-//         super(config);
-//         const load = this.load.bind(this);
-//         const audioChannel = this._audioChannel;
-//         // tslint:disable-next-line:no-shadowed-variable
-//         this.load = (context, config, callbacks) => {
-//             context.url = context.url.replace(/(\/seg-\d+-v\d+-a)\d+(\.ts)/i, '$1' + this._audioChannel + '$2');
-//             console.log('HlsCustomFLoader', this._audioChannel);
-//             const originalCallback = callbacks;
-//             const onSuccess = callbacks.onSuccess;
-//             // tslint:disable-next-line:no-shadowed-variable
-//             callbacks.onSuccess = (response, stats, context) => {
-//                 if ((response.data as ArrayBuffer).byteLength === 0) {
-//                     setTimeout(() => {
-//                         load(context, config, originalCallback);
-//                     }, 500);
-//                 } else {
-//                     onSuccess(response, stats, context);
-//                 }
-//             };
-//             load(context, config, callbacks);
-//         };
-//     }
-//
-//     private _audioChannel = 1;
-//
-//     get audioChannel(): number {
-//         return this._audioChannel;
-//     }
-//
-//     set audioChannel(value: number) {
-//         this._audioChannel = value;
-//     }
-// }
