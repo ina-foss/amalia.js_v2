9f8e0e28426450bfde0c93ee361bc4e913d2b125
mjelassi <mjelassi-prestataire@ina.fr>
2023-10-26 17:50:06 +0200

    fix tests

--- src/app/core/media-player-element.ts
+++ src/app/core/media-player-element.ts
@@ -238,6 +238,7 @@ export class MediaPlayerElement {
         } else if (this.width >= mWidth && this.width < lWidth) {
             displayState = 'm';
         }
+
         return displayState;
     }
 }
