4439d222a4133c54f3c2337b187d5bb5b5985fb4
drakulan <drakulan-prestataire@ina.fr>
2024-05-29 11:43:49 +0200

    Add 1h backward et forward

--- src/app/plugins/control-bar/control-bar-plugin.component.ts
+++ src/app/plugins/control-bar/control-bar-plugin.component.ts
@@ -558,6 +558,9 @@ export class ControlBarPluginComponent extends PluginBase<Array<ControlBarConfig
                 mediaPlayer.pause();
                 mediaPlayer.movePrevFrame(1);
                 break;
+            case 'backward-1h':
+                mediaPlayer.setCurrentTime((mediaPlayer.getCurrentTime() - 3600));
+                break;
             case 'backward-start':
                 this.changePlaybackRate(1);
                 mediaPlayer.seekToBegin();
@@ -577,6 +580,9 @@ export class ControlBarPluginComponent extends PluginBase<Array<ControlBarConfig
             case 'forward-10seconds':
                 frames = 10 * mediaPlayer.framerate;
                 mediaPlayer.moveNextFrame(frames);
+                break;
+            case 'forward-1h':
+                mediaPlayer.setCurrentTime((mediaPlayer.getCurrentTime() + 3600));
                 break;
             case 'forward-second':
                 frames = mediaPlayer.framerate;
